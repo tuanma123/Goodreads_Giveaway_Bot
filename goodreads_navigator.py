4c49231738a255eaa3118b072e0dded3b8e05fc3
Tuan Anh Ma <tuanma123@gmail.com>
2017-12-04 15:59:56 -0800

    Implemented 2 methods related to navigating to giveaway categories. 1. A method that returns a list of book genres. 2. A method that navigates to the page for genre.

--- goodreads_navigator.py
+++ goodreads_navigator.py
@@ -20,6 +20,30 @@ def setup_browser(is_headless):
     # Navigate to the Goodreads website and then
     browser.get("https://www.goodreads.com/user/sign_in?source=home")
 
+    # Input the username field.
+    username_location = browser.find_element_by_id("user_email")
+    username_location.send_keys(username)
+
+    # Input the password field.
+    password_location = browser.find_element_by_id("user_password")
+    password_location.send_keys(password)
+
+    # Send the information.
+    send_button = browser.find_element_by_tag_name("input").submit()
+
     return browser
 
-setup_browser(False)
+
+def get_genre_list():
+    genres = "Art, Biography, Business, Chick-lit, Children's, Christian, Classics, Comics, Contemporary, Cookbooks, " \
+             "Crime, Ebooks, Fantasy, Fiction, Gay and Lesbian, Graphic novels, Historical fiction, History, Horror, " \
+             "Humor and Comedy, Manga, Memoir, Music, Mystery, Non-fiction, Paranormal, Philosophy, Poetry, " \
+             "Psychology, Religion, Romance, Science, Science fiction, Self help, Spirituality, Sports, Suspense, " \
+             "Thriller, Travel, Young-adult".replace(", ", "!").replace(" ", "%20").split("!")
+    return genres
+
+def parse_genre(genre, browser):
+    browser.get("https://www.goodreads.com/giveaway/genre/" + genre)
+    print(browser.page_source)
+browser = setup_browser(False)
+parse_genre("Art", browser)
