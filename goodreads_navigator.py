658dc55abbf3030a27bf7ff4e23c4771d77b14f0
Tuan Anh Ma <tuanma123@gmail.com>
2017-12-04 22:01:14 -0800

    Major milestone. Got the basic functionality of the bot to work. Still a lot of issues that need to be addressed and the bot also should be smarter.

--- goodreads_navigator.py
+++ goodreads_navigator.py
@@ -29,7 +29,7 @@ def setup_browser(is_headless):
     password_location.send_keys(password)
 
     # Send the information.
-    send_button = browser.find_element_by_tag_name("input").submit()
+    browser.find_element_by_tag_name("input").submit()
 
     return browser
 
@@ -42,8 +42,59 @@ def get_genre_list():
              "Thriller, Travel, Young-adult".replace(", ", "!").replace(" ", "%20").split("!")
     return genres
 
-def parse_genre(genre, browser):
-    browser.get("https://www.goodreads.com/giveaway/genre/" + genre)
-    print(browser.page_source)
-browser = setup_browser(False)
-parse_genre("Art", browser)
+
+def get_genres_to_parse():
+    ignore_settings = open("ignore.txt", "r")
+    all_genres = get_genre_list()
+    genres_to_parse = []
+    for line in ignore_settings:
+        if line in all_genres:
+            all_genres.remove(line)
+    for genre in all_genres:
+        genres_to_parse.append("https://www.goodreads.com/giveaway/genre/" + genre + "?all_countries=true")
+    return genres_to_parse
+
+
+def get_href_list_page(browser, page_link):
+    browser.get(page_link)
+    giveaway_url_list = []
+    giveaway_list_elements = browser.find_elements_by_class_name("gr-button")
+    for item in giveaway_list_elements:
+        if item.get_attribute("href") is not None:
+            giveaway_url_list.append(item.get_attribute("href"))
+    return giveaway_url_list
+
+
+def get_all_href_list_genre(browser, root_link):
+    page_number = 1
+    giveaway_url_list = []
+    while True:
+        url = root_link + "&page=" + str(page_number)
+        giveaway_urls = get_href_list_page(browser, url)
+        if len(giveaway_urls) == 0:
+            break
+        else:
+            giveaway_url_list.extend(giveaway_urls)
+        page_number += 1
+    return giveaway_url_list
+
+
+def enter_giveaways(browser, giveaway_url_list):
+    for giveaway in giveaway_url_list:
+        browser.get(giveaway)
+
+        address = browser.find_element_by_id("addressSelect3262933").click()
+        want_to_read = browser.find_element_by_name("want_to_read").click()
+        entry_term = browser.find_element_by_name("entry_terms").click()
+        submit = browser.find_element_by_name("commit").click()
+
+def main():
+    browser = setup_browser(False)
+    all_urls = []
+    for genre in get_genres_to_parse():
+        all_urls.extend(get_all_href_list_genre(browser, genre))
+    enter_giveaways(browser, all_urls)
+
+main()
+
+
