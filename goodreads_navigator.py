b2a85d2e6c03010bda2393950262e450b64bdf2b
Tuan Anh Ma <tuanma123@gmail.com>
2017-12-09 22:50:20 -0800

    Several changes to the control flow of the script and some minor bug fixes. 1. Added an exception handling in the cases where the giveaway for a book does not have option for adding the book to the user's want to read list. 2. Fixed an error in reading the ignore settings. As it turns out it was not ignoring the categories it was supossed to because when reading the lines it included the newline characters that each line has. The solution is to replace any new line characters before comparing it to the value in the list of genres. 3. Deleted the part where I try to look at user configurations setting for deciding whether or not to add a book to the want to read section. It is was causing issue and is a feature I want to remove for now.
    Misc changes:
    1. More documentation for methods mostly.

--- goodreads_navigator.py
+++ goodreads_navigator.py
@@ -1,8 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 import configparser
 
+config = configparser.ConfigParser()
+config.read("settings.ini")
+
 
 def setup_browser(is_headless):
+    """
+    This method setups the Chrome based Selenium browser that logs into Goodreads using the login information stored in
+    the settings file.
+
+    :param is_headless: A boolean value that indicates whether the browser is headless or not i.e. does the browser
+    display graphically.
+    :return: The browser that the method creates after it attempts to login.
+    """
+
     # Setup your browser object and its options.
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'credentials_enable_service': False})
@@ -11,9 +24,7 @@ def setup_browser(is_headless):
         options.add_argument("headless")
     browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
 
-    # Setup a parser for the config
-    config = configparser.ConfigParser()
-    config.read("settings.ini")
+    # Setup the username and password from the config file.
     username = config["credentials"]["username"]
     password = config["credentials"]["password"]
 
@@ -35,6 +46,11 @@ def setup_browser(is_headless):
 
 
 def get_genre_list():
+    """
+    This method returns a list of all valid genres on Goodreads.
+
+    :return: A list of string containing the genres.
+    """
     genres = "Art, Biography, Business, Chick-lit, Children's, Christian, Classics, Comics, Contemporary, Cookbooks, " \
              "Crime, Ebooks, Fantasy, Fiction, Gay and Lesbian, Graphic novels, Historical fiction, History, Horror, " \
              "Humor and Comedy, Manga, Memoir, Music, Mystery, Non-fiction, Paranormal, Philosophy, Poetry, " \
@@ -44,12 +60,26 @@ def get_genre_list():
 
 
 def get_genres_to_parse():
+    """
+    This method calculates which genres' giveaways to enter based on user set ignore preferences.
+
+    :return: A list of URL to genre pages whoses giveaways are to be entered.
+    """
+
+    # Open the ignore settings file for reading.
     ignore_settings = open("ignore.txt", "r")
+    # Get a list of all avaiable genres in Goodreads.
     all_genres = get_genre_list()
+    # Create a list to store the URL for each of the valid genres.
     genres_to_parse = []
+
+    # Go through every line in the ignore settings file and remove them from the genres list if they are in the file.
     for line in ignore_settings:
+        line = str(line).replace("\n", "")
         if line in all_genres:
             all_genres.remove(line)
+
+    # Go through every valid genre and create the URL from them. Return the list after that.
     for genre in all_genres:
         genres_to_parse.append("https://www.goodreads.com/giveaway/genre/" + genre + "?all_countries=true")
     return genres_to_parse
@@ -80,21 +110,36 @@ def get_all_href_list_genre(browser, root_link):
 
 
 def enter_giveaways(browser, giveaway_url_list):
-    for giveaway in giveaway_url_list:
-        browser.get(giveaway)
+    """
+    This method takes a passed in giveaway URL and enters said giveaway.
 
-        address = browser.find_element_by_id("addressSelect3262933").click()
-        want_to_read = browser.find_element_by_name("want_to_read").click()
-        entry_term = browser.find_element_by_name("entry_terms").click()
-        submit = browser.find_element_by_name("commit").click()
+    :param browser: The browser to use to navigate.
+    :param giveaway_url_list: The URL address of the giveaway.
+    """
+    for giveaway in giveaway_url_list:
+        try:
+            # Navigate to the correct URL.
+            browser.get(giveaway)
+
+            # Select the first cached address on the user's account.
+            browser.find_element_by_id("addressSelect3262933").click()
+
+            browser.find_element_by_name("want_to_read").click()
+            # Click the button that agrees to the terms of entry.
+            browser.find_element_by_name("entry_terms").click()
+            # Click the submit button to enter the giveaway.
+            browser.find_element_by_name("commit").click()
+        except(NoSuchElementException):
+            continue
 
 def main():
     browser = setup_browser(False)
     all_urls = []
     for genre in get_genres_to_parse():
         all_urls.extend(get_all_href_list_genre(browser, genre))
-    enter_giveaways(browser, all_urls)
+        enter_giveaways(browser, all_urls)
+        all_urls.clear()
+    browser.close()
+
 
 main()
-
-
