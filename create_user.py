78b386704d531acecdc2bdb895677549d1bbe1fb
Tuan Anh Ma <tuanma123@gmail.com>
2018-01-20 20:26:06 -0800

    Added a script to create a new user.

---
+++ create_user.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+"""
+This scripts provides an utility for users to easily create settings files for the main script to
+use.
+"""
+import os
+import goodreads_navigator as navigator
+
+print("Welcome. This script helps you create a new user setting file. Escape using your system's "
+      "escape sequence to close (ctrl + c for Linux).")
+username = input("Please input a Goodreads account username.")
+while os.path.exists(username + ".ini"):
+    username = input("Username configuration already exists, please enter a new one.")
+password = input("Please input the corresponding account password.")
+country = input("Please input the code for your country (e.g. enter US for the United States).")
+logging = input("Would you like the bot to create log files whilst parsing (y/n)?")
+logging = logging == "y"
+headless = input("Would you like the browser to graphically display whilst the bot "
+                 "runs(y/n)?")
+headless = headless == "y"
+ignore_list = []
+print("Printing a list of genres. Please enter any categories you wish to ignore. Enter \"quit\' to"
+      "continue.")
+for genre in navigator.get_genre_list():
+    print(genre.replace("%20", " "))
+ignore = ""
+while not ignore.lower() == "quit":
+    ignore = input()
+    if ignore.replace(" ", "%20") not in navigator.get_genre_list():
+        print("Invalid input. Ignoring.")
+        continue
+    ignore_list.append(ignore)
+
+settings_file = open(username + ".ini", "w")
+settings_file.write("[credentials]")
+settings_file.write("username=" + username)
+settings_file.write("password=" + password)
+settings_file.write("country=" + country)
+settings_file.write("[settings]")
+settings_file.write("headless=" + headless)
+settings_file.write("logging=" + logging)
+settings_file.write("[ignore]")
+for x in range(0, len(ignore_list)):
+    settings_file.write(str(x) + "=" + ignore_list[x])
